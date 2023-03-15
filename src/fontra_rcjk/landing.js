218d018a18e419c96f1e5adeb0c96a033c7ff09d
nedkamburov <nedislav.kamburov@gmail.com>
2023-03-15 10:59:30 +0200

    cleanup

--- src/fontra_rcjk/landing.js
+++ src/fontra_rcjk/landing.js
@@ -15,8 +15,7 @@ function rcjkAuthenticateFunc() {
   const logoutButton = document.querySelector("#logout-button");
   const loginFailureMessage = document.querySelector("#login-failure-message");
 
-  const username = cookies["fontra-username"];
-  console.log(decodeURI(username, "UTF-8"));
+  const username = decodeURI(cookies["fontra-username"], "UTF-8");
   const haveToken = !!cookies["fontra-authorization-token"];
   const loginFailed = cookies["fontra-authorization-failed"] == "true";
 
