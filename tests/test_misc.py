edaab24c8f8cd00abe37ca2ac34dc475abb6710e
Just van Rossum <justvanrossum@gmail.com>
2023-05-05 14:03:17 +0200

    Test safe name algo

---
+++ tests/test_misc.py
@@ -0,0 +1,20 @@
+import pytest
+
+from fontra_rcjk.base import makeSafeLayerName
+
+
+@pytest.mark.parametrize(
+    "layerName, expectedSafeLayerName",
+    [
+        ("a", "a"),
+        ("a" * 50, "a" * 50),
+        ("á" * 50, "á" * 50),
+        ("a" * 51, "a" * 37 + ".bfc5fe0e3601"),
+        ("á" * 51, "á" * 37 + ".3c1a18fbe650"),
+        ("a/b", "a_b.c14cddc033f6"),
+        ("a+b", "a_b.300273daf0bb"),
+    ],
+)
+def test_safeLayerName(layerName, expectedSafeLayerName):
+    safeLayerName = makeSafeLayerName(layerName)
+    assert expectedSafeLayerName == safeLayerName
