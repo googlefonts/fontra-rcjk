e1b26bdd295ab9c2aaa58bef78a409b337210c76
Just van Rossum <justvanrossum@gmail.com>
2023-11-17 10:16:29 +0100

    Use cattrs; from_dict disappeared with the removal of dacite

--- tests/test_font.py
+++ tests/test_font.py
@@ -4,6 +4,7 @@ import shutil
 from dataclasses import asdict
 from importlib.metadata import entry_points
 
+import cattrs
 import pytest
 from fontra.core.classes import (
     GlobalAxis,
@@ -13,7 +14,6 @@ from fontra.core.classes import (
     Source,
     StaticGlyph,
     VariableGlyph,
-    from_dict,
 )
 
 from fontra_rcjk.base import makeSafeLayerName
@@ -396,7 +396,7 @@ async def test_getGlyphMap(backendName, numGlyphs, testMapping):
 @pytest.mark.asyncio
 @pytest.mark.parametrize("backendName, expectedGlyph", getGlyphTestData)
 async def test_getGlyph(backendName, expectedGlyph):
-    expectedGlyph = from_dict(VariableGlyph, expectedGlyph)
+    expectedGlyph = cattrs.structure(expectedGlyph, VariableGlyph)
     font = getTestFont(backendName)
     with contextlib.closing(font):
         glyph = await font.getGlyph(expectedGlyph.name)
