77eb2739002c7cdb733d671df47a2bf19ed3c574
Olli Meier <hallo@olli-meier.de>
2024-03-06 16:26:55 +0100

    redo unicodes for GLIFGlyph

--- src/fontra_rcjk/base.py
+++ src/fontra_rcjk/base.py
@@ -28,7 +28,7 @@ FONTRA_STATUS_KEY = "fontra.development.status"
 class GLIFGlyph:
     def __init__(self):
         self.name = None  # Must be set to a string before we can write GLIF data
-        self.codePoints = []
+        self.unicodes = []
         self.width = 0
         self.path = None
         self.lib = {}
