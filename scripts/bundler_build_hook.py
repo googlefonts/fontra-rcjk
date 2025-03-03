2a8f5ef1549d9dc9dcd2bbe3e94d86a3475f8874
Simon Cozens <simon@simon-cozens.org>
2025-03-03 17:39:34 +0000

    Use main fontra repo and --prefer-dedupe

--- scripts/bundler_build_hook.py
+++ scripts/bundler_build_hook.py
@@ -7,5 +7,5 @@ from hatchling.builders.hooks.plugin.interface import BuildHookInterface
 
 class RollupBuildHook(BuildHookInterface):
     def initialize(self, version, build_data):
-        subprocess.check_output("npm install", shell=True)
+        subprocess.check_output("npm install --prefer-dedupe", shell=True)
         subprocess.check_output("npm run bundle", shell=True)
